c972ba13778120d1e47c3671fb79b6133cbf2349
DekingChen <13570490483@163.com>
2016-05-04 18:31:30 +0800

    add api package

---
+++ DekingChen/src/dk/api/API.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package dk.api;
+
+/**
+ * @author Administrator
+ *
+ */
+public class API {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
