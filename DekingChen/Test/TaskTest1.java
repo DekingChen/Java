16fa4f8f7e093d7f49068d6b015b71e2f4c2bfc8
DekingChen <13570490483@163.com>
2016-06-21 18:52:05 +0800

    add junit examples

---
+++ DekingChen/Test/TaskTest1.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+
+public class TaskTest1 {
+	@Test
+	public void test() {
+		System.out.println("this is TaskTest1...");
+	}
+}
---
+++ DekingChen/Test/TaskTest2.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+
+public class TaskTest2 {
+
+
+	@Test
+	public void test() {
+		System.out.println("this is TaskTest2...");
+	}
+
+}
